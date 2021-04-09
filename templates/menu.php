0340a91d0dfd43ca3c59d157ccd9e6b68243aeed
Keciot <Keciot@gmail.com>
2021-04-10 01:42:35 +0300

    Настройка автогенерации меню

--- templates/menu.php
+++ templates/menu.php
@@ -1,3 +1,21 @@
-<a href="#">Главая</a>
-<a href="/?page=catalog">Каталог</a>
-<br><br>
+<?php 
+$menuArr = array(
+    'Главная' => '',
+    'Каталог' => '/?page=catalog',
+    'Контакты' => '/?page=contacts',
+    'Пункт меню' => /* array(
+        'Подменю 1' => '/?page=submenu1',
+        'Подменю 2' =>  */'/?page=submenu2'
+    /* )  Для подменю фокус пока не удался, пусть полежит заготовка*/
+);
+
+function menuConstructor($array) {
+    echo '<ul>';
+    foreach($array as $key => $value):?>
+        <li><a href="<?=$value?>"><?=$key?></a></li>
+    <?php endforeach;
+     echo '</ul>';
+}
+
+menuConstructor($menuArr);
+?>
